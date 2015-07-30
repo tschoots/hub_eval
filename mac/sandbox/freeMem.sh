7dc9f11a2a2a820372b9382f875d50233c5ed67e
Ton Schoots <tschoots@blackducksoftware.com>
2015-07-30 12:26:31 +0200

    dynamically determine the minumum between 75% of ram or free mem to use as a max to start

--- mac/sandbox/freeMem.sh
+++ mac/sandbox/freeMem.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+
+# determine the max memory by taking the minimum of 75% of total memory and the amount of free memory
 free_pages=$(vm_stat | awk '/Pages free:/ {print $3}' | sed 's/\.//')
 page_size_bytes=$(vm_stat | awk '/page size of/ {print $8}')
-
 free_mem_mb=$(awk "BEGIN {printf \"%.0f\", $(($free_pages * $page_size_bytes))/1048576}")
 
 max_mem=$(sysctl hw.memsize | awk '{print $2}')
 max_mem=$(awk "BEGIN {printf \"%.0f\", $(($max_mem / 1048576))*0.75}")  # take 75% of total memory
 
-mem=8192
+echo $max_mem
+if [ $free_mem_mb -lt $max_mem ];then
+   max_mem=$free_mem_mb
+fi
 
-# Set the container memory
-mem_set_cmd="VBoxManage modifyvm boot2docker-vm --memory ${mem}"
-echo $mem_set_cmd
-
+echo $max_mem
 echo $free_pages
 echo $page_size_bytes
 echo $free_mem_mb
--- mac/start.sh
+++ mac/start.sh
@@ -45,8 +45,19 @@ function checkHubRunning {
 
 port=$_DEFAULT_PORT
 
+# determine the max memory by taking the minimum of 75% of total memory and the amount of free memory
+free_pages=$(vm_stat | awk '/Pages free:/ {print $3}' | sed 's/\.//')
+page_size_bytes=$(vm_stat | awk '/page size of/ {print $8}')
+free_mem_mb=$(awk "BEGIN {printf \"%.0f\", $(($free_pages * $page_size_bytes))/1048576}")
+
 max_mem=$(sysctl hw.memsize | awk '{print $2}')
 max_mem=$(awk "BEGIN {printf \"%.0f\", $(($max_mem / 1048576))*0.75}")  # take 75% of total memory
+
+echo $max_mem
+if [ $free_mem_mb -lt $max_mem ];then
+   max_mem=$free_mem_mb
+fi
+
 cpus=$(sysctl kern.aioprocmax | awk '{print $2}')
 max_cpus=$(awk "BEGIN {printf \"%.0f\", $(($cpus))*0.5}")  # take 75% of total memory
 
